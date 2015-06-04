8cf65f6672ba07f9dd80deb7f459a4e2a8f0952b
mclucario <mclucario@freenet.de>
2015-06-04 22:17:01 +0200

    Main Changes

---
+++ SkypeMessageTool/Form1.Designer.vb
@@ -0,0 +1,248 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.ListBox3 = New System.Windows.Forms.ListBox()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(10, 19)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(163, 303)
+        Me.ListBox1.TabIndex = 0
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(354, 19)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(163, 303)
+        Me.ListBox2.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(179, 19)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(169, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = ">>"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(179, 48)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(169, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = ">"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(179, 77)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(169, 23)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "<"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(179, 106)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(169, 23)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "<<"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(10, 449)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(507, 23)
+        Me.Button5.TabIndex = 6
+        Me.Button5.Text = "Send"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 3)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(52, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Contacts:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(351, 3)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(95, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Gets the message:"
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(-2, 0)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(534, 505)
+        Me.TabControl1.TabIndex = 10
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.RichTextBox1)
+        Me.TabPage1.Controls.Add(Me.ListBox1)
+        Me.TabPage1.Controls.Add(Me.Button4)
+        Me.TabPage1.Controls.Add(Me.Label1)
+        Me.TabPage1.Controls.Add(Me.ListBox2)
+        Me.TabPage1.Controls.Add(Me.Button3)
+        Me.TabPage1.Controls.Add(Me.Button2)
+        Me.TabPage1.Controls.Add(Me.Label2)
+        Me.TabPage1.Controls.Add(Me.Button5)
+        Me.TabPage1.Controls.Add(Me.Button1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(526, 479)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Skype Mass Message"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(10, 328)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(507, 115)
+        Me.RichTextBox1.TabIndex = 10
+        Me.RichTextBox1.Text = "Enter Message to send here."
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.Label3)
+        Me.TabPage2.Controls.Add(Me.Button6)
+        Me.TabPage2.Controls.Add(Me.RichTextBox2)
+        Me.TabPage2.Controls.Add(Me.ListBox3)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(526, 479)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "List Message Sender"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(10, 14)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(104, 13)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "Skype Contact Lists:"
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(10, 446)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(507, 23)
+        Me.Button6.TabIndex = 12
+        Me.Button6.Text = "Send"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.Location = New System.Drawing.Point(10, 325)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.Size = New System.Drawing.Size(507, 115)
+        Me.RichTextBox2.TabIndex = 11
+        Me.RichTextBox2.Text = "Enter Message to send here."
+        '
+        'ListBox3
+        '
+        Me.ListBox3.FormattingEnabled = True
+        Me.ListBox3.Location = New System.Drawing.Point(10, 30)
+        Me.ListBox3.Name = "ListBox3"
+        Me.ListBox3.Size = New System.Drawing.Size(507, 290)
+        Me.ListBox3.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(531, 503)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Skype Message Tool"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents ListBox2 As System.Windows.Forms.ListBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
+    Friend WithEvents ListBox3 As System.Windows.Forms.ListBox
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents RichTextBox2 As System.Windows.Forms.RichTextBox
+
+End Class
---
+++ SkypeMessageTool/Form1.vb
@@ -0,0 +1,173 @@
+﻿Imports SKYPE4COMLib
+
+Public Class Form1
+
+    Dim Skype As New SKYPE4COMLib.Skype
+    Dim ArrayBuffer As Array
+    Dim HandleBuffer As String
+    Dim NameBuffer As String
+    Dim counter = 0
+
+    Private Sub TabControl1_TabIndexChanged(sender As Object, e As EventArgs) Handles TabControl1.TabIndexChanged
+
+        ListBox1.Items.Clear()
+        ListBox2.Items.Clear()
+
+        If TabControl1.SelectedIndex = 0 Then
+
+            For Each User As SKYPE4COMLib.User In Skype.Friends
+
+                ListBox1.Items.Add(User.Handle + ", " + User.FullName)
+
+            Next
+
+        End If
+
+        If TabControl1.SelectedIndex = 0 Then
+
+            For Each Group As SKYPE4COMLib.Group In Skype.Groups
+
+                ListBox3.Items.Add(Group.DisplayName)
+
+            Next
+
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        counter = ListBox1.Items.Count - 1
+
+        While counter > -1
+
+            ListBox2.Items.Add(ListBox1.Items.Item(counter))
+            ListBox1.Items.Remove(ListBox1.Items.Item(counter))
+
+            counter -= 1
+
+        End While
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        If ListBox1.SelectedItems.Count = 1 Then
+
+            ListBox2.Items.Add(ListBox1.SelectedItem)
+            ListBox1.Items.Remove(ListBox1.SelectedItem)
+
+        End If
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        If ListBox2.SelectedItems.Count = 1 Then
+
+            ListBox1.Items.Add(ListBox2.SelectedItem)
+            ListBox2.Items.Remove(ListBox2.SelectedItem)
+
+        End If
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        counter = ListBox2.Items.Count - 1
+
+        While counter > -1
+
+            ListBox1.Items.Add(ListBox2.Items.Item(counter))
+            ListBox2.Items.Remove(ListBox2.Items.Item(counter))
+
+            counter -= 1
+
+        End While
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+        SendMessageSMM()
+
+    End Sub
+
+    Sub SendMessageSMM()
+
+        If ListBox2.Items.Count = 0 Then
+
+            MsgBox("You haven't selected any contacts which should get the message.", MsgBoxStyle.Exclamation, "No contacts")
+
+            Exit Sub
+
+        End If
+
+        If RichTextBox1.Text = "" Then
+
+            MsgBox("You haven't written anything into the textbox.", MsgBoxStyle.Exclamation, "No message")
+
+            Exit Sub
+
+        End If
+
+        Try
+
+            For Each Item In ListBox2.Items
+
+                ArrayBuffer = Item.ToString.Split(CChar(","))
+                HandleBuffer = ArrayBuffer(0)
+                NameBuffer = ArrayBuffer(1).ToString.Remove(0, 1)
+
+                Skype.SendMessage(HandleBuffer, RichTextBox1.Text.Replace("%contact%", NameBuffer))
+
+            Next
+
+        Catch ex As Exception
+
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "Error")
+
+        End Try
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+
+        If ListBox3.Items.Count = 0 Then
+
+            MsgBox("You haven't selected any contacts which should get the message.", MsgBoxStyle.Exclamation, "No contacts")
+
+            Exit Sub
+
+        End If
+
+        If RichTextBox2.Text = "" Then
+
+            MsgBox("You haven't written anything into the textbox.", MsgBoxStyle.Exclamation, "No message")
+
+            Exit Sub
+
+        End If
+
+        Try
+
+            For Each Group As SKYPE4COMLib.Group In Skype.Groups
+
+
+
+                For Each User As SKYPE4COMLib.User In Group.Users
+
+
+                Next
+
+            Next
+
+        Catch ex As Exception
+
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "Error")
+
+        End Try
+
+    End Sub
+
+End Class
